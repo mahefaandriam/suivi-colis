68b1695856f9e13c06cd67dbd2cdc7a76c11f49e
mahefaandriam <fenoandriams@gmail.com>
2025-11-25 16:02:41 +0300

    ds

--- src/hooks/usePublicDeliveries.ts
+++ src/hooks/usePublicDeliveries.ts
@@ -2,17 +2,6 @@
 import { useQuery } from 'react-query';
 import { publicDeliveryApi } from '../services/publicApi';
 
-export const usePublicDelivery = (id: string) => {
-  return useQuery(
-    ['public-delivery', id],
-    () => publicDeliveryApi.getDeliveryById(id),
-    {
-      enabled: !!id,
-      retry: false,
-    }
-  );
-};
-
 export const usePublicDeliveryByTracking = (trackingNumber: string, logedEmail: string) => {
   return useQuery(
     ['public-delivery', trackingNumber],
